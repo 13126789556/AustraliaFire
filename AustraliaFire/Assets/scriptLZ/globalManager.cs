6c4fff75de01ff7440432bcd6b43a6631fee60ab
Luyao Zhao <Luyaoz3283@gmail.com>
2020-01-20 17:49:15 -0800

    UI_v1

---
+++ AustraliaFire/Assets/scriptLZ/globalManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalManager : MonoBehaviour
+{
+    public enum actionList {fightFire, cleanWater};
+    [HideInInspector]public actionList curAction;
+
+    public int curMoney;
+    public int curPeople;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curMoney = 5000;
+        curPeople = 10;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
